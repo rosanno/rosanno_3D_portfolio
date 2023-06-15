22e3db3757c1c7cdf16d1d415d53b7231e5f22f7
rosanno <rosannomacabuhay@gmail.com>
2023-06-15 19:02:50 +0800

    Added linkedIn profile link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion";
-import { BsDownload, BsGithub } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { AiOutlineLinkedin } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
@@ -13,18 +14,38 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm{" "}
-           <AnimatePresence>
-              {["R","o","s","a","n","n","o", " ", "M","a","c","a","b","u","h","a","y"].map((text,idx) => (
-                  <motion.span key={idx}  
-                    initial={{ opacity: 0, x: -50 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: 50 }}
-                    transition={{ duration: 0.5, delay: idx * 0.3 }} className="text-[#E94560] lg:text-5xl inline"
-                  >
-                    {text}
-                  </motion.span>
+            <AnimatePresence>
+              {[
+                "R",
+                "o",
+                "s",
+                "a",
+                "n",
+                "n",
+                "o",
+                " ",
+                "M",
+                "a",
+                "c",
+                "a",
+                "b",
+                "u",
+                "h",
+                "a",
+                "y",
+              ].map((text, idx) => (
+                <motion.span
+                  key={idx}
+                  initial={{ opacity: 0, x: -50 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 50 }}
+                  transition={{ duration: 0.5, delay: idx * 0.3 }}
+                  className="text-[#E94560] lg:text-5xl inline"
+                >
+                  {text}
+                </motion.span>
               ))}
-           </AnimatePresence>
+            </AnimatePresence>
           </h1>
           <p className={`${styles.heroSubText} text-white-100`}>
             Aspiring Software Engineer
@@ -38,9 +59,13 @@ const Hero = () => {
               <BsGithub className="text-lg" />
               <span>github</span>
             </Link>
-            <Link to="/" className="resume flex items-center gap-3 mt-7">
-              <BsDownload className="text-lg" />
-              <span>resume</span>
+            <Link
+              to="https://www.linkedin.com/in/rosanno-macabuhay-a8469227b/"
+              target="_blank"
+              className="resume flex items-center gap-3 mt-7"
+            >
+              <AiOutlineLinkedin className="text-lg" />
+              <span>linkedIn</span>
             </Link>
           </div>
         </div>
