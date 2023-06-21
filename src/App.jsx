f01011dbdc811eaed453ed26b7d8fd45a369aed8
rosanno <rosannomacabuhay@gmail.com>
2023-06-21 20:59:18 +0800

    Adjust padding for small devices

--- src/App.jsx
+++ src/App.jsx
@@ -57,25 +57,23 @@ const App = () => {
         <BrowserRouter>
           <div className="bg-primary">
             <div
-              className={`fixed inset-x-0 z-20 px-24 py-5 ${
+              className={`fixed inset-x-0 z-20 px-3 md:px-12 xl:px-24 py-5 ${
                 scrolled ? "bg-primary" : "bg-transparent"
               }`}
             >
               <AnimatePresence>
-                {["s", "a", "n", "n", "o", ".", "d", "e", "v"].map(
-                  (text, idx) => (
-                    <motion.span
-                      initial={{ opacity: 0, y: 50 }}
-                      animate={{ opacity: 1, y: 0 }}
-                      exit={{ opacity: 0, y: 50 }}
-                      transition={{ duration: 0.5, delay: idx * 0.3 }}
-                      key={idx}
-                      className="uppercase inline-block text-rose-500 font-bold"
-                    >
-                      {text}
-                    </motion.span>
-                  )
-                )}
+                {["s", "a", "n", "n", "o", ".", "d", "e", "v"].map((text, idx) => (
+                  <motion.span
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 50 }}
+                    transition={{ duration: 0.5, delay: idx * 0.3 }}
+                    key={idx}
+                    className="uppercase inline-block text-rose-500 font-bold"
+                  >
+                    {text}
+                  </motion.span>
+                ))}
               </AnimatePresence>
             </div>
             <div className="relative z-0">
